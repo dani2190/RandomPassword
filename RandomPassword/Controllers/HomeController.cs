6b251592a235bd9f9695e817485f3af002d11ca1
Daniel Gatabazi <danielgatabazi@Daniels-MacBook-Air.local>
2020-08-10 11:51:01 -0500

    initial

---
+++ RandomPassword/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RandomPassword.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace RandomPassword.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            int? counter = HttpContext.Session.GetInt32("counter");
+            counter = (counter == null) ? 0 : counter;
+            counter++;
+
+            ViewBag.Count = counter;
+            ViewBag.passcode = RandomString(14);
+            HttpContext.Session.SetInt32("counter", (int)counter);
+            return View();
+        }
+        [Route("/getpass")]
+        [HttpGet]
+        public JsonResult GetPAss()
+        {
+            int? counter = HttpContext.Session.GetInt32("counter");
+            counter = (counter == null) ? 1 : counter;
+            counter++;
+
+            // ViewBag.Count = counter;
+            ViewBag.passcode = RandomString(14);
+            HttpContext.Session.SetInt32("counter", (int)counter);
+            var AnonObject = new
+            {
+                count = counter,
+                passcode = ViewBag.passcode,
+            
+            };
+            return Json(AnonObject);
+
+        
+        }
+    }
+    }
